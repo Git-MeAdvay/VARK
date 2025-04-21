f7162e5b953a0177cea26ee4fcabd3ec9c2e9ab9
Git-MeAdvay <advaydongre@gmail.com>
2025-04-21 16:31:16 +0530

    Made arrangements in Marsthi quiz as per in English quiz.

---
+++ frontend/src/test_data/ILS.js
@@ -0,0 +1,58 @@
+const ilsQuestions = [
+    {
+      question: "I understand something better after I",
+      options: [
+        { text: "try it out", type: "ACT" },
+        { text: "think it through", type: "REF" }
+      ]
+    },
+    {
+      question: "When I am learning something new, it helps me to",
+      options: [
+        { text: "talk about it", type: "ACT" },
+        { text: "think about it", type: "REF" }
+      ]
+    },
+    {
+      question: "I prefer courses that emphasize",
+      options: [
+        { text: "concrete material (facts, data)", type: "SEN" },
+        { text: "abstract material (concepts, theories)", type: "INT" }
+      ]
+    },
+    {
+      question: "I find it easier to",
+      options: [
+        { text: "learn facts", type: "SEN" },
+        { text: "learn concepts", type: "INT" }
+      ]
+    },
+    {
+      question: "When I think about what I did yesterday, I am most likely to get",
+      options: [
+        { text: "a picture", type: "VIS" },
+        { text: "words", type: "VRB" }
+      ]
+    },
+    {
+      question: "I prefer to get new information in",
+      options: [
+        { text: "pictures, diagrams, graphs, or maps", type: "VIS" },
+        { text: "written directions or verbal information", type: "VRB" }
+      ]
+    },
+    {
+      question: "I tend to",
+      options: [
+        { text: "understand details of a subject but may be fuzzy about its overall structure", type: "SEQ" },
+        { text: "understand the overall structure but may be fuzzy about details", type: "GLO" }
+      ]
+    },
+    {
+      question: "When solving problems I",
+      options: [
+        { text: "work my way to the solutions one step at a time", type: "SEQ" },
+        { text: "often just see the solutions but then have to struggle to figure out the steps", type: "GLO" }
+      ]
+    }
+  ];
